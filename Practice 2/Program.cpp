78566f7bb1ed41bf3160d8339a04fb590fcf2389
Ali Seifi <seifiali84@gmail.com>
2024-02-09 23:36:18 +0330

    Add Practice 2

---
+++ Practice 2/Program.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // Ali Seifi ASCII Codes : 65 108 105 32 83 101 105 102 105
+    cout << char(65) << char(108) << char(105) << char(32) << char(83) << char(101) << char(105) << char(102) << char(105) << "\n";
+    return 0;
+}
